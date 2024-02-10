8fedb713f1d3e24a1c8ed8680f87a328d6e669fc
jbier <jbier2010@gmail.com>
2024-02-10 15:29:47 -0500

    exercise 3

--- build.sh
+++ build.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 echo "Cleaning existing classes..."
-rm -f *.class
-# This command looks for matching files and runs the rm command for each file
-# Note that {} are replaced with each file name
-find . -name \*.class -exec rm {} \;
+rm -rf build
+mkdir build
 
 echo "Compiling source code and unit tests..."
-javac -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -d build -classpath .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar src/main/java/*.java src/test/java/*.java
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
-java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore EdgeConnectorTest
+java -cp build:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore src/test/java/EdgeConnectorTest
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java -classpath . RunEdgeConvert
+java -classpath build:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar RunEdgeConvert
